8644952fb2f0e401317cb72c899d0e5fa1fc8fef
SHINDONGWOOK <copanote@gmail.com>
2018-02-03 11:14:45 +0900

    [Thread] Single Threaded Execution modify gate class to thread safe

--- java/designThread/SingleThreadedExecution/Gate.java
+++ java/designThread/SingleThreadedExecution/Gate.java
@@ -1,18 +1,19 @@
 package SingleThreadedExecution;
 
-//Thread Unsafe
+//ThreadSafe
 public class Gate {
 	private int counter = 0;
 	private String name = "Nobody";
 	private String address = "Nowhere";
-	public void pass(String name, String address) {
+	public synchronized void pass(String name, String address) {
 		this.counter++;
 		this.name = name;
 		this.address = address;
 		check();
 	}
 	
-	public String toString() {
+	//debug message 출력할 떄 race condition 발생
+	public synchronized String toString() {
 		return "No." + counter + ": " + name + ", " + address;
 	}
 	
