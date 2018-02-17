8425561543f30d93281b3fb6cf31b1a374059aa5
copanote <copanote@gmail.com>
2018-02-18 00:17:03 +0900

    [Thread] Future

---
+++ java/designThread/Future/FutureData.java
@@ -0,0 +1,26 @@
+package Future;
+
+public class FutureData implements Data {
+	private RealData realdata = null;
+	private boolean ready = false;
+	public synchronized void setRealData(RealData realdata) {
+		if(ready) {
+			return; //balk
+		}
+		this.realdata = realdata;
+		this.ready = true;
+		notifyAll();
+	}
+
+	@Override
+	public synchronized String getContent() {
+		while(!ready) {
+			try {
+				wait();
+			}catch(InterruptedException e) {}
+		}
+		
+		return realdata.getContent();
+	}
+
+}
